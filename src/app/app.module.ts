2f8c9b4a302f68cc2113c43ad832379ac9adc282
Infinter <33516713+Infinter@users.noreply.github.com>
2017-11-29 13:49:24 +0100

    Section 4 session 18

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-
+import { RouterModule } from '@angular/router'
 
 
 import { AppComponent } from './app.component';
@@ -11,7 +11,17 @@ import { JobListComponent } from './job-list/job-list.component';
 import { JobService } from './services/job.service';
 import { JobAddFormComponent } from './job-add-form/job-add-form.component';
 import { DaysAgoPipe } from './pipes/days-ago.pipe';
+import { HomeComponent } from './home/home.component';
+import { JobDetailsComponent } from './job-details/job-details.component';
+import { AboutComponent } from './about/about.component';
 
+const routes = [
+  { path: '', component:HomeComponent},
+  { path: 'jobs/add', component:JobAddFormComponent },
+  { path: 'jobs/:id', component:JobDetailsComponent},
+  { path: 'jobs', component:JobListComponent},
+  { path: 'about', component:AboutComponent },
+]
 
 @NgModule({
   declarations: [
@@ -19,13 +29,17 @@ import { DaysAgoPipe } from './pipes/days-ago.pipe';
     SearchComponent,
     JobListComponent,
     JobAddFormComponent,
-    DaysAgoPipe
+    DaysAgoPipe,
+    HomeComponent,
+    JobDetailsComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
     HttpModule,
     FormsModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [JobService],
   bootstrap: [AppComponent]
--- src/app/services/job.service.ts
+++ src/app/services/job.service.ts
@@ -1,24 +1,45 @@
 import { Injectable } from '@angular/core';
 import { Http } from '@angular/http';
 import 'rxjs/add/operator/map';
-import { Subject} from "rxjs/Rx";
+import 'rxjs/add/operator/do';
+import { Subject } from "rxjs/Rx";
+import { Observable } from 'rxjs/Observable';
 
 @Injectable()
 export class JobService {
-
+  initialJobs = [];
   jobs = [];
   jobsSubject = new Subject();
 
   constructor(private http: Http) { }
 
   getJobs() {
-    return this.http.get('data/jobs.json')
-      .map(res => res.json());
+    if (this.jobs.length > 0 && this.initialJobs.length > 0) {
+      console.log("case if");
+      return Observable.of([...this.jobs, ...this.initialJobs]);
+
+    } else if (this.jobs.length > 0 && this.initialJobs.length === 0) {
+      console.log('case du else if');
+      return this.http.get('data/jobs.json')
+        .map(res => res.json())
+        .do(data => {
+          this.initialJobs = data;
+          this.jobs = [...this.jobs, ...this.initialJobs];
+        })
+      
+    } else {
+      console.log('case du else');
+      return this.http.get('data/jobs.json')
+        .map(res => res.json())
+        .do(data => this.initialJobs = data);
+    }
   }
 
-  addJob(jobData){
+  addJob(jobData) {
     jobData.id = Date.now();
+    this.jobs = [jobData, ...this.jobs]
     return this.jobsSubject.next(jobData);
   }
+
 
 }
